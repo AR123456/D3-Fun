f38ac2a5b49f739e814a0f6b427f4dafd15f1a4b
AR123456 <coloradoz99@gmail.com>
2019-02-14 04:43:04 -0700

    selecting elements with d3

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// // vanilla javascript
+// const a = document.querySelector("div");
+// // same thing with the d3 methond from its library plus it puts it in a d3 wrapper so other d3 methods are avalible
+// const b = d3.select("div");
+
+// // vanilla javascript
+// const a = document.querySelectorAll("div");
+// // same thing with the d3 methond from its library plus it puts it in a d3 wrapper so other d3 methods are avalible
+// const b = d3.selectAll("div");
+
+// console.log(a, b);
+
+const canvas = d3.select(".canvas");
