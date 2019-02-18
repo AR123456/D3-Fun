e9702ae0e7bad4b974212752f4c60f0a6bfe11a4
AR123456 <coloradoz99@gmail.com>
2019-02-18 06:12:47 -0700

    method chaiing and attributes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,39 @@
 const canvas = d3.select(".canvas");
 // append is an d3 methond that appends the svg to the canvas
-const svg = canvas.append("svg");
-// append shapes to svg container
-svg.append("rect");
-svg.append("circle");
-svg.append("line");
 
-//can also append p tags and other elements
+// ///////////const svg = canvas.append("svg");
+// // can use attr to add attributes to the svg
+// svg.attr("height", 600);
+// svg.attr("width", 600);
+// ////////but this can also be done like this
+// the return value is the ellement appended const svg = canvas.append("svg")
+const svg = canvas
+  .append("svg")
+  .attr("height", 600)
+  .attr("width", 600);
+
+// append shapes to svg container
+svg
+  .append("rect")
+  .attr("width", 200)
+  .attr("height", 100)
+  .attr("fill", "blue")
+  .attr("x", 20)
+  .attr("y", 20);
+
+svg
+  .append("circle")
+  .attr("r", 50)
+  .attr("cx", 300)
+  .attr("cy", 70)
+  .attr("fill", "pink");
+
+svg
+  .append("line")
+  .attr("x1", 370)
+  .attr("x2", 400)
+  .attr("y1", 120)
+  .attr("y2", 20)
+  .attr("stroke", "red");
+
+//can also append p tags and other elements just will not on this course
