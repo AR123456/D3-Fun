c67e70789b4fad18811f3f0616f8470bf0f203e0
AR123456 <coloradoz99@gmail.com>
2019-02-21 05:08:47 -0700

    D# Joining Data to element

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,35 @@
-//d3 expects data in array format, to do more that one rectangle pass more rectangle objects into the array
-// in real life may not know how many rects there will ultimatly be ..
-//d3 sees all the items in this array but as virtual selections, they still need to be joined to the DOM - Enter Selection
+//d3 expects data in array format
 const data = [
   {
     width: 200,
-    height: 100,
-    fill: "purple"
-  },
-  {
-    width: 100,
-    height: 60,
-    fill: "pink"
-  },
-  {
-    width: 50,
-    height: 30,
+    height: 400,
     fill: "red"
   }
 ];
 
+//always start off with a reff to the svg - this is were will append to
 const svg = d3.select("svg");
-//this joins the data to rects
+//select rectangle inside of the svg
+const rect = svg
+  .select("rect")
+  //here using the .data method from d3 and in the params is the const data above
+  .data(data)
+  // create a function that returns a value instead of a hard coded number
+  //the d is for data
+  // the function can also take in  i and n
+  // i is the index of the current element inside of the array
+  //n is the current selection in this case the rectangle
+  .attr("width", function(d, i, n) {
+    // console.log(d);
+    // console.log(i);
+    // console.log(n);
+    return d.width;
+  })
+  .attr("height", function(d) {
+    return d.height;
+  })
+  .attr("fill", function(d) {
+    return d.fill;
+  });
 
-const rects = svg.selectAll("rect").data(data);
-
-//this updates the rect element in the dom
-rects
-  .attr("width", (d, i, n) => d.width)
-  .attr("height", d => d.height)
-  .attr("fill", d => d.fill);
-//this updates the stuff that has not yet entered the DOM via the enter selection
-rects
-  .enter()
-  .append("rect")
-  .attr("width", (d, i, n) => d.width)
-  .attr("height", d => d.height)
-  .attr("fill", d => d.fill);
+// console.log(rect);
