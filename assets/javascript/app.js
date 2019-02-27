334308d87662772156680edab1235abf8fad03f0
AR123456 <coloradoz99@gmail.com>
2019-02-27 04:47:05 -0700

    band scale

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,8 @@
+/// band scales space out categories in a bar chart
+// the scale of the width of the rectangles so that no matter how many come in as data the all fit into the svg
+// use d3 band scale  - splits data in to bands of equeal value
+// pass  in names and x coord, where it starts on x axis  and it returns the band width based on how many items and the width avalible in the svg container
+
 const data = [
   {
     name: "veg soup",
@@ -16,13 +21,23 @@ const data = [
     orders: 900
   }
 ];
-//create a linear scale using d3.scaleLinear
-//pass domain - the input values between( the max values)  and   range
+
 const y = d3
   .scaleLinear()
-  // this is essentialy making it 1/2 scale
   .domain([0, 1000])
   .range([0, 500]);
+
+const x = d3
+  .scaleBand()
+  // array of the different categories use map
+  .domain(data.map(item => item.name))
+  // console.log(data.map(item => item.name));
+  // now pass the range , an array of 2 points representing the x range  0-500
+  .range([0, 500])
+  //put padding between bars
+  .paddingInner(0.2)
+  // put padding at begging and end of the chart
+  .paddingOuter(0.2);
 
 /// select svg container first
 const svg = d3.select("svg");
@@ -30,22 +45,18 @@ const svg = d3.select("svg");
 const rects = svg.selectAll("rect").data(data);
 //update recs in dom
 rects
-  .attr("width", 50)
-  //need to pass the height through the y scale so enclose d function into y
-  // .attr("height", d => d.orders)
+  // the bandWidth  methond returns the value of the width of each bar
+  .attr("width", x.bandwidth)
   .attr("height", d => y(d.orders))
   .attr("fill", "orange")
-  // shifting tho the right 70 px for each rectangle
-  .attr("x", (d, i) => i * 70);
+  .attr("x", d => x(d.name));
 
-//append the enter selection to the DOM
+//append the enter selection from d3 virtuyal to the DOM
 rects
   .enter()
   .append("rect")
-  .attr("width", 50)
-  //need to pass the height through the y scale so enclose d function into y
-  // .attr("height", d => d.orders)
+  // the bandWidth  methond returns the value of the width of each bar
+  .attr("width", x.bandwidth)
   .attr("height", d => y(d.orders))
   .attr("fill", "orange")
-  // shifting tho the right 70 px for each rectangle
-  .attr("x", (d, i) => i * 70);
+  .attr("x", d => x(d.name));
