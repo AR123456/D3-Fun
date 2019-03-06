9983405b15c73c19e95e4c10a1d90712ab472d5e
AR123456 <coloradoz99@gmail.com>
2019-03-06 07:31:20 -0700

    set up files for fitness tracker

--- index.js
+++ index.js
@@ -1,39 +1,32 @@
-//interacting with firebase and DOM go here
-// get elements on page
-// add event listioner for submining form
-// chekc to make sure there are values enterred
-//create object
-// add the object to the dem
+// //interacting with firebase and DOM
+// const form = document.querySelector("form");
+// const cost = document.querySelector("#cost");
+// const name = document.querySelector("#name");
+// const error = document.querySelector("#error");
 
-const form = document.querySelector("form");
-const cost = document.querySelector("#cost");
-const name = document.querySelector("#name");
-const error = document.querySelector("#error");
+// form.addEventListener("submit", e => {
+//   // prevent default action (page refresh)
+//   e.preventDefault();
+//   //check that name and cost have been entered
+//   if (name.value && cost.value) {
+//     // create the object () documment )to submit to firebase
+//     const item = {
+//       name: name.value,
+//       cost: parseInt(cost.value)
+//     };
 
-form.addEventListener("submit", e => {
-  // prevent default action (page refresh)
-  e.preventDefault();
-  //check that name and cost have been entered
-  if (name.value && cost.value) {
-    // create the object () documment )to submit to firebase
-    const item = {
-      name: name.value,
-      cost: parseInt(cost.value)
-    };
+//     db.collection("expenses")
+//       .add(item)
+//       .then(res => {
+//         // clear the error message if there is one
+//         error.textContent = "";
+//         //after getting response reset the input fields
+//         name.value = "";
+//         cost.value = "";
+//       });
+//   } else {
+//     error.textContent = "Please enter values before submitting";
+//   }
 
-    //submit the object to the db ( use .then casue it is a promis )
-    db.collection("expenses")
-      .add(item)
-      .then(res => {
-        // clear the error message if there is one
-        error.textContent = "";
-        //after getting response reset the input fields
-        name.value = "";
-        cost.value = "";
-      });
-  } else {
-    error.textContent = "Please enter values before submitting";
-  }
-
-  //
-});
+//   //
+// });
