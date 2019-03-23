b9d3256fa0e8f704194e388562cedfb61f38e418
AR123456 <coloradoz99@gmail.com>
2019-03-23 08:10:53 -0600

    enter lines selection

--- graph.js
+++ graph.js
@@ -1,75 +1,100 @@
-const dims = { height: 500, width: 1500 };
+const dims = { height: 500, width: 1100 };
 
-const svg = d3
-  .select(".canvas")
-  .append("svg")
-  .attr("width", dims.width + 100)
-  .attr("height", dims.height + 100);
+const svg = d3.select('.canvas')
+  .append('svg')
+  .attr('width', dims.width + 100)
+  .attr('height', dims.height + 100);
 
-const graph = svg.append("g").attr("transform", "translate(50, 50)");
+const graph = svg.append('g')
+  .attr('transform', 'translate(50, 50)');
 
 // tree and stratify
-const tree = d3.tree().size([dims.width, dims.height]);
-
-const stratify = d3
-  .stratify()
+const stratify = d3.stratify()
   .id(d => d.name)
   .parentId(d => d.parent);
 
-// update function
-const update = data => {
+const tree = d3.tree()
+  .size([dims.width, dims.height]);
+
+// update function  
+const update = (data) => {
+
+  // remove current nodes
+  graph.selectAll('.node').remove();
+  graph.selectAll('.link').remove();
+
   // get updated root Node data
   const rootNode = stratify(data);
-  const treeData = tree(rootNode);
+  const treeData = tree(rootNode).descendants();
+  
+  // get nodes selection and join data
+  const nodes = graph.selectAll('.node')
+    .data(treeData);
 
-  // console.log(treeData);
+  // get link selection and join new data
+  const link = graph.selectAll('.link')
+    .data(tree(rootNode).links());
 
-  // get nodes selection and join new data
-  const nodes = graph.selectAll(".node").data(tree(rootNode).descendants());
-  //enter node groups
-  const enterNodes = nodes
-    .enter()
-    .append("g")
-    .attr("class", "node")
-    .attr("transform", d => `translate(${d.x}, ${d.y})`);
+  // enter new links
+  link.enter()
+    .append('path')
+      .transition().duration(300)
+      .attr('class', 'link')
+      .attr('fill', 'none')
+      .attr('stroke', '#aaa') 
+      .attr('stroke-width', 2)
+      .attr('d', d3.linkVertical()
+        .x(d => d.x)
+        .y(d => d.y )
+      );
+
+  // create enter node groups
+  const enterNodes = nodes.enter()
+    .append('g')
+      .attr('class', 'node')
+      .attr('transform', d => `translate(${d.x}, ${d.y})`);
+      
   // append rects to enter nodes
-  enterNodes
-    .append("rect")
-    .attr("fill", "#aaa")
-    .attr("stroke", "#555")
-    .attr("stroke-width", 2)
-    .attr("height", 50)
-    .attr("width", d => d.data.name.length * 20);
-  // append name text
-  enterNodes
-    .append("text")
-    .attr("text-anchor", "middle")
-    .attr("fill", "white")
-    .text(d => d.data.name);
+  enterNodes.append('rect')
+    .attr('fill', '#aaa')
+    .attr('stroke', '#555')
+    .attr('stroke-width', 2)
+    .attr('width', d => d.data.name.length * 20)
+    .attr('height', 50);
+
+  enterNodes.append('text')
+    .attr('text-anchor', 'middle')
+    .attr('fill', 'white')
+    .text(d => d.data.name); 
+
 };
 
 // data & firebase hook-up
 var data = [];
 
-db.collection("employees").onSnapshot(res => {
+db.collection('employees').onSnapshot(res=> {
+
   res.docChanges().forEach(change => {
-    const doc = { ...change.doc.data(), id: change.doc.id };
+
+    const doc = {...change.doc.data(), id: change.doc.id};
 
     switch (change.type) {
-      case "added":
+      case 'added':
         data.push(doc);
         break;
-      case "modified":
+      case 'modified':
         const index = data.findIndex(item => item.id == doc.id);
         data[index] = doc;
         break;
-      case "removed":
+      case 'removed':
         data = data.filter(item => item.id !== doc.id);
         break;
       default:
         break;
     }
+
   });
 
   update(data);
+
 });
