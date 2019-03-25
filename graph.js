e4016e3b98218bde5ae372fbff484327474f7871
AR123456 <coloradoz99@gmail.com>
2019-03-25 05:46:36 -0600

    buget planner on click

--- graph.js
+++ graph.js
@@ -1,111 +1,176 @@
-const dims = { height: 500, width: 1100 };
+// interactive D3 visualizaitons - onclicks, double-clicks hovers, drags, tooltips ect
+//Section 9 lecture 66
+
+// Event listenters - D3 supports most vanilla JS events
+//https://developer.mozilla.org/en-US/docs/Web/Events
+// syntax for setting up event listeners
+// make selection of the ellements
+//const rects = d3.selectAll("rects")
+//then use the on metond to attach an event the first parameter is the type of event , then declare the call back function
+//  rects.on("click", function)  can be an arrow function  or if you want to use 'this' inside it can be old school function
+
+/////////////////////// adding a mouse over event
+const dims = { height: 300, width: 300, radius: 150 };
+
+const cent = { x: dims.width / 2 + 5, y: dims.height / 2 + 5 };
 
 const svg = d3
   .select(".canvas")
   .append("svg")
-  .attr("width", dims.width + 100)
-  .attr("height", dims.height + 100);
+  .attr("width", dims.width + 150)
+  .attr("height", dims.height + 150);
 
-const graph = svg.append("g").attr("transform", "translate(50, 50)");
+const graph = svg
+  .append("g")
+  .attr("transform", `translate(${cent.x}, ${cent.y})`);
+const pie = d3
+  .pie()
+  .sort(null)
+  .value(d => d.cost);
 
-// tree and stratify
-const stratify = d3
-  .stratify()
-  .id(d => d.name)
-  .parentId(d => d.parent);
+const arcPath = d3
+  .arc()
+  .outerRadius(dims.radius)
+  .innerRadius(dims.radius / 2);
 
-const tree = d3.tree().size([dims.width, dims.height]);
+const colour = d3.scaleOrdinal(d3["schemeSet3"]);
+// //// legend set up /////////////////
+const legendGroup = svg
+  .append("g")
+  .attr("transform", `translate(${dims.width + 40}, 10)`);
+//legendColor comes from the plug in
+const legend = d3
+  .legendColor()
+  .shape("circle")
+  .shapePadding(10)
+  .scale(colour);
 
-// create ordinal scale here - set scale ot const and set range of colors to comeoutn of the const
-const colorScale = d3.scaleOrdinal(d3["schemeCategory10"]);
-
-// update function
+////////////////////////the update function ///////////////////
 const update = data => {
-  // remove current nodes
-  graph.selectAll(".node").remove();
-  graph.selectAll(".link").remove();
-  // create the domain of the ordinal scale- array of names (strings) and passin
-  colorScale.domain(data.map(d => d.name));
-  // get updated root Node data
-  const rootNode = stratify(data);
-  const treeData = tree(rootNode).descendants();
+  colour.domain(data.map(d => d.name));
+  // update and call legend
+  legendGroup.call(legend);
+  legendGroup.selectAll("text").attr("fill", "white");
 
-  // get nodes selection and join data
-  const nodes = graph.selectAll(".node").data(treeData);
+  const paths = graph.selectAll("path").data(pie(data));
 
-  // get link selection and join new data
-  const link = graph.selectAll(".link").data(tree(rootNode).links());
+  // handle the exit selection
+  paths
+    .exit()
+    .transition()
+    .duration(750)
+    .attrTween("d", arcTweenExit)
+    .remove();
+  // handles the current DOM path updates
+  paths
+    .attr("d", arcPath)
+    .transition()
+    .duration(750)
+    .attrTween("d", arcTweenUpdate);
 
-  // enter new links
-  link
+  paths
     .enter()
     .append("path")
+    .attr("class", "arc")
+    .attr("stroke", "#fff")
+    .attr("stroke-width", 3)
+    .attr("fill", d => colour(d.data.name))
+    .each(function(d) {
+      this._current = d;
+    })
     .transition()
-    .duration(300)
-    .attr("class", "link")
-    .attr("fill", "none")
-    .attr("stroke", "#aaa")
-    .attr("stroke-width", 2)
-    .attr(
-      "d",
-      d3
-        .linkVertical()
-        .x(d => d.x)
-        .y(d => d.y)
-    );
-
-  // create enter node groups
-  const enterNodes = nodes
-    .enter()
-    .append("g")
-    .attr("class", "node")
-    .attr("transform", d => `translate(${d.x}, ${d.y})`);
-
-  // append rects to enter nodes
-  enterNodes
-    .append("rect")
-    // use ordinal scale for fill color here
-    // .attr("fill", "#aaa")
-    .attr("fill", d => colorScale(d.data.name))
-    .attr("stroke", "#555")
-    .attr("stroke-width", 2)
-    .attr("width", d => d.data.name.length * 20)
-    .attr("height", 50)
-    .attr("transform", (d, i, n) => {
-      let x = d.data.name.length * 10;
-      return `translate(${-x}, -25)`;
-    });
-
-  enterNodes
-    .append("text")
-    .attr("text-anchor", "middle")
-    .attr("dy", 5)
-    .attr("fill", "white")
-    .text(d => d.data.name);
+    .duration(750)
+    .attrTween("d", arcTweenEnter);
+  // first step select the elements , in this case the pies are paths so that is the element to select, need to do it in 'enter' since it is at this point where they are on the page
+  graph
+    .selectAll("path")
+    //////// adding events //////
+    // then add the event listener and the call back function
+    // the call back function is being handed the 'd' (the data on the path)  as  well as the 'i'- index of that element in the selection  and 'n'- the array of elements in that selection
+    .on("mouseover", handleMouseOver)
+    .on("mouseout", handleMouseOut)
+    // adding an on click to del a slice of the pie
+    .on("click", handleClick);
 };
-
-// data & firebase hook-up
 var data = [];
 
-db.collection("employees").onSnapshot(res => {
-  res.docChanges().forEach(change => {
-    const doc = { ...change.doc.data(), id: change.doc.id };
-
-    switch (change.type) {
-      case "added":
-        data.push(doc);
-        break;
-      case "modified":
-        const index = data.findIndex(item => item.id == doc.id);
-        data[index] = doc;
-        break;
-      case "removed":
-        data = data.filter(item => item.id !== doc.id);
-        break;
-      default:
-        break;
-    }
+db.collection("expenses")
+  .orderBy("cost")
+  .onSnapshot(res => {
+    res.docChanges().forEach(change => {
+      const doc = { ...change.doc.data(), id: change.doc.id };
+      switch (change.type) {
+        case "added":
+          data.push(doc);
+          break;
+        case "modified":
+          const index = data.findIndex(item => item.id == doc.id);
+          data[index] = doc;
+          break;
+        case "removed":
+          data = data.filter(item => item.id !== doc.id);
+          break;
+        default:
+          break;
+      }
+    });
+    update(data);
   });
 
-  update(data);
-});
+const arcTweenEnter = d => {
+  var i = d3.interpolate(d.endAngle, d.startAngle);
+  return function(t) {
+    d.startAngle = i(t);
+    return arcPath(d);
+  };
+};
+
+const arcTweenExit = d => {
+  var i = d3.interpolate(d.startAngle, d.endAngle);
+  return function(t) {
+    d.startAngle = i(t);
+    return arcPath(d);
+  };
+};
+function arcTweenUpdate(d) {
+  var i = d3.interpolate(this._current, d);
+  this._current = i(1);
+  return function(t) {
+    return arcPath(i(t));
+  };
+}
+//// this is the call back function in the on click event
+/// event handlers
+// using es6 as an alternative
+const handleMouseOver = (d, i, n) => {
+  // console.log("this is the element being hovered over: ", n[i]);
+  //wrap n[i] in the d3.select so that we have access to d3 methods
+  d3.select(n[i])
+    //adding d3 methods
+    //maing this a named transition
+    // .transition()
+    .transition("changeSliceFill")
+    .duration(300)
+    .attr("fill", "#fff");
+};
+const handleMouseOut = (d, i, n) => {
+  // console.log("this is the element being hovered over: ", n[i]);
+  //wrap n[i] in the d3.select so that we have access to d3 methods
+  d3.select(n[i])
+    //adding d3 methods
+
+    //maing this a named transition
+    // .transition()
+    .transition("changeSliceFill")
+    .duration(300)
+    .attr("fill", colour(d.data.name));
+};
+
+const handleClick = d => {
+  // console.log(d);
+  // in this object is the firestore of the thing that was clicked on .
+  const id = d.data.id;
+  db.collection("expenses")
+    .doc(id)
+    .delete();
+};
--- index.js
+++ index.js
@@ -1,23 +1,39 @@
-const modal = document.querySelector('.modal');
-M.Modal.init(modal);
+//interacting with firebase and DOM go here
+// get elements on page
+// add event listioner for submining form
+// chekc to make sure there are values enterred
+//create object
+// add the object to the dem
 
-const form = document.querySelector('form');
-const name = document.querySelector('#name');
-const parent = document.querySelector('#parent');
-const department = document.querySelector('#department');
+const form = document.querySelector("form");
+const cost = document.querySelector("#cost");
+const name = document.querySelector("#name");
+const error = document.querySelector("#error");
 
-form.addEventListener('submit', e => {
+form.addEventListener("submit", e => {
+  // prevent default action (page refresh)
   e.preventDefault();
+  //check that name and cost have been entered
+  if (name.value && cost.value) {
+    // create the object () documment )to submit to firebase
+    const item = {
+      name: name.value,
+      cost: parseInt(cost.value)
+    };
 
-  db.collection('employees').add({
-    name: name.value, 
-    parent: parent.value, 
-    department: department.value
-  });
+    //submit the object to the db ( use .then casue it is a promis )
+    db.collection("expenses")
+      .add(item)
+      .then(res => {
+        // clear the error message if there is one
+        error.textContent = "";
+        //after getting response reset the input fields
+        name.value = "";
+        cost.value = "";
+      });
+  } else {
+    error.textContent = "Please enter values before submitting";
+  }
 
-  var instance = M.Modal.getInstance(modal);
-  instance.close();
-
-  form.reset();
-
+  //
 });
