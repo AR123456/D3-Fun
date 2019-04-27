2f5e51b3e837162cd31558b6e2e0f46a675be970
AR123456 <coloradoz99@gmail.com>
2019-04-27 11:29:30 -0600

    data exit selection

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,10 @@
 //the data that will be used for the app
-
 var quotes = [
   {
     quote: "I see dead people.",
     movie: "The Sixth Sense",
     year: 1999,
-    rating: "PG-13"
+    rating: "PG13"
   },
   {
     quote: "May the force be with you.",
@@ -34,40 +33,57 @@ var quotes = [
     rating: "G"
   }
 ];
+var newQuotes = [
+  {
+    quote: "Houston, we have a problem.",
+    movie: "Apollo 13",
+    year: 1995,
+    rating: "PG13"
+  },
+  {
+    quote: "Gentlemen, you can't fight in here! This is the war room!",
+    movie:
+      "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
+    year: 1964,
+    rating: "PG"
+  }
+];
 
 // this  object converts the ratings to objects
 var colors = {
   G: "#3cff00",
   PG: "#f9ff00",
-  "PG-13": "#ff900",
+  PG13: "#ff9000",
   R: "#ff0000"
 };
-
 //style the list and add the movie quotes to the page
 d3.select("#quotes")
   .style("list-style", "none")
-  //select the selection oject
   .selectAll("li")
-  //take each item in the array and attach it to a dom element
   .data(quotes)
-  // the . data ender and exit properties
   .enter()
   .append("li")
-  // old school way
-  // .text(function(d) {
-  //   //set the text of each item with a call back function
-  //   return '"' + d.quote + '"-' + d.movie + "(" + d.year + ")";
-  // })
-  //es6
   .text(d => '"' + d.quote + '"-' + d.movie + "(" + d.year + ")")
   .style("margin", "20px")
   .style("padding", "20px")
-  //old school way
-  // .style("font-size", function(d) {
-  //   return d.quote.length < 25 ? "2em" : "1em";
-  // });
-  //es6 way
   .style("font-size", d => (d.quote.length < 25 ? "2em" : "1em"))
-  // use the colors object to set the background color of the quote based on rating
   .style("background-color", d => colors[d.rating])
   .style("border=radius", "8px");
+
+// code to remove quotes 
+var removeBtn = d3.select("#remove");
+
+removeBtn.on('click', function() {
+  var nonRQuotes = quotes.filter(function(movie) {
+    return movie.rating !== 'R';
+  });
+  
+  d3.selectAll("li")
+    .data(nonRQuotes, function(d) {
+      return d.quote;
+    })
+    .exit()
+    .remove();
+
+  removeBtn.remove();
+});
