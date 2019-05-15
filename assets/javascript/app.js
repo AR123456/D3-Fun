21339f05b0aa86ff71175c844ebdf5391dc7e486
AR123456 <coloradoz99@gmail.com>
2019-05-15 05:32:27 -0600

    adding axi to scatter plot

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 var width = 500;
 var height = 500;
-var padding = 20;
+var padding = 30;
 // use extent for this
 // var yMax = d3.max(birthData, d => d.lifeExpectancy);
 // var yMin = d3.min(birthData, d => d.lifeExpectancy);
@@ -12,6 +12,16 @@ var xScale = d3
   .scaleLinear()
   .domain(d3.extent(birthData, d => d.births / d.population))
   .range([padding, width - padding]);
+var xAxis = d3
+  .axisBottom(xScale)
+  //this adds grid lines by extending the tick lines out
+  .tickSize(-height + 2 * padding)
+  .tickSizeOuter(0);
+var yAxis = d3
+  .axisLeft(yScale)
+  //this adds grid lines by extending the tick lines out
+  .tickSize(-width + 2 * padding)
+  .tickSizeOuter(0);
 var colorScale = d3
   .scaleLinear()
   .domain(d3.extent(birthData, d => d.population / d.area))
@@ -20,6 +30,16 @@ var radiusScale = d3
   .scaleLinear()
   .domain(d3.extent(birthData, d => d.births))
   .range([2, 40]);
+d3.select("svg")
+  .append("g")
+  // move the axi to lower left
+  .attr("transform", "translate(0, " + (height - padding) + ")")
+  .call(xAxis);
+d3.select("svg")
+  .append("g")
+  // move the axi to lower left
+  .attr("transform", "translate(" + padding + ",0)")
+  .call(yAxis);
 
 d3.select("svg")
   .attr("width", width)
@@ -34,3 +54,35 @@ d3.select("svg")
   .attr("fill", d => colorScale(d.population / d.area))
 
   .attr("r", d => radiusScale(d.births));
+// adding text, no secret sauce for this need to use d3 fundementals
+//select SVG, append text element, position correclty  and set inner text
+d3.select("svg")
+  // center horizontally and verticaly with the x axis
+  .append("text")
+  .attr("x", width / 2)
+  .attr("y", height - padding)
+  // D-y attribute to push down relative ot the x axis
+  .attr("dy", "1.5em")
+  .style("text-anchor", "middle")
+  .text("Births per Capita ");
+// center text relative to x attribute  and set text inside
+d3.select("svg")
+  // center horizontally and verticaly  with the x axis
+  .append("text")
+  .attr("x", width / 2)
+  .attr("y", padding)
+  .style("text-anchor", "middle")
+  .style("font-size", "1.5em")
+  .text("Data on Births by Country in 2011 ");
+
+// setting the y axis - need to rotate text 90 degrees
+d3.select("svg")
+  .append("text")
+  .attr("transform", "rotate(-90)")
+
+  // x attribute will move text element in vertical direction
+  .attr("x", -height / 2)
+  .attr("y", padding)
+  .attr("dy", "-1.1em")
+  .style("text-anchor", "middle")
+  .text("Life Expectancy");
