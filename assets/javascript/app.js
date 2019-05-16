6dfd3dc3a9e4dde52ceabd2be1d92b3878c0b7b0
AR123456 <coloradoz99@gmail.com>
2019-05-16 06:19:16 -0600

    D3 SVG and HIstogram

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,102 +1,77 @@
 var width = 600;
 var height = 600;
-var padding = 50;
-// feed in the regionData  object and apply the filer method to the data coming back from the musHaveKeys function
-var data = regionData.filter(mustHaveKeys);
-//literacy rate
+var barPadding = 1;
+// get just first year
+var minYear = d3.min(birthData, d => d.year);
+// store first year in a variable
+var yearData = birthData.filter(d => d.year === minYear);
+// create an x scale so that rectangles with more or less data are displayed with differing widths
+// define this prior to defining the histogram function
 var xScale = d3
   .scaleLinear()
-  //finding the smallest and largest literacy rates using extent
-  .domain(d3.extent(data, d => d.adultLiteracyRate))
-  // using ragne methond to pass array - map to svg bounds
-  .range([padding, width - padding]);
-//cell subscriber per 100
+  // scale from zero to the largest value in the data set
+  .domain([0, d3.max(yearData, d => d.births)])
+  // range is 0 to the svg width the range round method rounds the widths to the nearest whole number
+  .rangeRound([0, width]);
+
+// create a histogram variable and set it to d3.histogram generator
+var histogram = d3
+  // the histogram method is called on the histogram generator, can be used to specify domain of values to use to create bins.If no domain is specified will default to using extent of data set
+  .histogram()
+  // getting domain originally used to create scale with .domain
+  .domain(xScale.domain())
+  // the thresholds method overides the default ranges on each band
+  // xScale.ticks returns an array of evenly spaced intermediate values
+  .thresholds(xScale.ticks())
+  // histogram of birth counts
+  .value(d => d.births);
+// pass data into the histogram generator and store it in the bin variable
+//bins is an array of 2 array elements with birth data plus the properties x0 and x1
+// from this it is known how many rectangles are needed and what the y scale should be
+var bins = histogram(yearData);
+
+var barWidth = width / bins.length - barPadding;
+// using scale linear to calculate thw width
 var yScale = d3
   .scaleLinear()
-  .domain(d3.extent(data, d => d.subscribersPer100))
-  .range([height - padding, padding]);
-// using radious to vizualize the median age- need a scale for this
-var rScale = d3
-  .scaleLinear()
-  .domain(d3.extent(data, d => d.medianAge))
-  .range([5, 30]);
-// fill to vizualize the urban population rate - need a scale for this
-var fScale = d3
-  .scaleLinear()
-  .domain(d3.extent(data, d => d.urbanPopulationRate))
-  .range(["green", "blue"]);
-// axis variables to store d3 Axis s
-var xAxis = d3
-  .axisBottom(xScale)
-  .tickSize(-height + 2 * padding)
-  .tickSizeOuter(0);
-
-var yAxis = d3
-  .axisLeft(yScale)
-  .tickSize(-width + 2 * padding)
-  .tickSizeOuter(0);
-// setting the SVG variable
-var svg = d3
+  // 0 to the length of the largest bin
+  .domain([0, d3.max(bins, d => d.length)])
+  //svg hight to to zero
+  .range([height, 0]);
+// select svg - set width and height
+var bars = d3
   .select("svg")
   .attr("width", width)
-  .attr("height", height);
-// append group  to the page for the x axis
-svg
-  .append("g")
-  .attr("transform", "translate(0," + (height - padding) + ")")
-  .call(xAxis);
-//append group to page for the y axis
-svg
-  .append("g")
-  .attr("transform", "translate(" + padding + ",0)")
-  .call(yAxis);
-// adding text label to x
-svg
-  .append("text")
-  .attr("x", width / 2)
-  .attr("y", height - padding)
-  .attr("dy", padding / 2)
-  .style("text-anchor", "middle")
-  .text("Literacy Rate, Aged 15 and Up");
-// add text label to y - need to rotate the y
-svg
-  .append("text")
-  .attr("transform", "rotate(-90)")
-  .attr("x", -height / 2)
-  .attr("dy", padding / 2)
-  .style("text-anchor", "middle")
-  .text("Cellular Subscribers per 100 People");
-// using the D3 "pattern" to add circles to the page from the SVG
-svg
-  .selectAll("circle")
-  .data(data)
+  .attr("height", height)
+  // join data to elements with a class of bar
+  .selectAll(".bar")
+  .data(bins)
   .enter()
-  .append("circle")
-  // using scales already built inside the callbacks
-  .attr("cx", d => xScale(d.adultLiteracyRate))
-  .attr("cy", d => yScale(d.subscribersPer100))
-  .attr("r", d => rScale(d.medianAge))
-  .attr("fill", d => fScale(d.urbanPopulationRate))
-  //giving circles a white outline
-  .attr("stroke", "#fff");
-// adding the title to the graph
-svg
+  // for each enter node append a g element
+  .append("g")
+  // need 2 elements for each svg in the data point- rectangle and text element with description of data
+  .classed("bar", true);
+// inside the group append the histogram rectangles
+bars
+  // this is the bar chart pattern
+  .append("rect")
+  // set the attributes so that each rectangle is offset by the bar width plus the bar padding for the y attribute
+  // using the zero and x1 properties of bin to calculate bar width
+  .attr("x", (d, i) => xScale(d.x0))
+  // for y attribute use scale
+  .attr("y", d => yScale(d.length))
+  // height wil be = to the svg height less the scaled value
+  .attr("height", d => height - yScale(d.length))
+  // width will be the bar width
+  .attr("width", d => xScale(d.x1) - xScale(d.x0) - barPadding)
+  .attr("fill", "#9c27b0");
+// text labels of each bar chart
+bars
   .append("text")
-  .attr("x", width / 2)
-  .attr("dy", "1em")
-  .style("text-anchor", "middle")
-  .style("font-size", "2em")
-  .text("Cellular Subscriptions vs. Literacy Rate");
-// a helper function to handle  the 4 objects on intrest and check for objects without values on each key , if no  value return false
-function mustHaveKeys(obj) {
-  var keys = [
-    "subscribersPer100",
-    "adultLiteracyRate",
-    "medianAge",
-    "urbanPopulationRate"
-  ];
-  for (var i = 0; i < keys.length; i++) {
-    if (obj[keys[i]] === null) return false;
-  }
-  return true;
-}
+  .text(d => d.x0 + " - " + d.x1 + " (bar height: " + d.length + ")")
+  //rotate 90 deg so that text runs lenth of bar
+  .attr("transform", "rotate(-90)")
+  // align to show in middle of bar and at bottom of svg
+  .attr("y", d => (xScale(d.x1) + xScale(d.x0)) / 2)
+  .attr("x", -height + 10)
+  .style("alignment-baseline", "middle");
