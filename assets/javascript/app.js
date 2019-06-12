1c5d54eea8b11f5ed74c266864cc233cae44159d
AR123456 <coloradoz99@gmail.com>
2019-06-12 05:50:24 -0600

    D3 dragging nodes and alpah values

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,7 @@ var nodes = [
   { color: "blue", size: 25 },
   { color: "purple", size: 30 }
 ];
-// each link object must have a force and target
+
 var links = [
   { source: "red", target: "orange" },
   { source: "orange", target: "yellow" },
@@ -31,13 +31,23 @@ var linkSelection = svg
   .append("line")
   .attr("stroke", "black")
   .attr("stroke-width", 1);
+// add the drag call back to the nodeSelection
 var nodeSelection = svg
   .selectAll("circle")
   .data(nodes)
   .enter()
   .append("circle")
   .attr("r", d => d.size)
-  .attr("fill", d => d.color);
+  .attr("fill", d => d.color)
+  //d3.drags 3 elements
+  .call(
+    d3
+      .drag()
+      // she second parameter is a function defined below
+      .on("start", dragStart)
+      .on("drag", drag)
+      .on("end", dragEnd)
+  );
 
 var simulation = d3.forceSimulation(nodes);
 
@@ -50,21 +60,44 @@ simulation
     d3
       .forceLink(links)
       .id(d => d.color)
-      //need to translate the source and target since de assumes that they are indices.
-      // using id method and specify D3 find based on color.
-      //strength and force between links ins determined by distance ans strength.  Interactions with other forces can cause actual distance to vary from ideal lenthg.
-      //strength attribute is proportional to the diff between the lengths actual distance and its desired distance  like a spring - the more its streched or compressed the more it wants to return to original length
-      //using the node data from the simulation so that larger nodes have greater repulsive force when connected by links to smaller nodes
+
       .distance(d => 5 * (d.source.size + d.target.size))
   )
   .on("tick", ticked);
 
 function ticked() {
+  /// conosle.log the alpha value
+  // console.log(simulation.alpha());
   nodeSelection.attr("cx", d => d.x).attr("cy", d => d.y);
-  // need to set this dynamically the node data from the simulation can be used , updating the properties here
+
   linkSelection
     .attr("x1", d => d.source.x)
     .attr("y1", d => d.source.y)
     .attr("x2", d => d.target.x)
     .attr("y2", d => d.target.y);
+}
+// pass "d" into the functions
+function dragStart(d) {
+  console.log("starting to drag");
+  // could work with alpha value here and in drag end to manipulate the target and cause simulation to warm back up as alpha goes towards new target
+  simulation.alphaTarget(0.5).restart();
+  // set the fx and fy properties for the start position
+  d.fx = d.x;
+  d.fy = d.y;
+}
+function drag(d) {
+  console.log("dragging");
+  // could forcibly set a new alpha value inside drag event then restart the simulation, don't pick a value greater than one or the simulation will break
+  // simulation.alpha(0.5).restart();
+  // update the properties base on the event
+  d.fx = d3.event.x;
+  d.fx = d3.event.y;
+}
+function dragEnd(d) {
+  console.log("drag ended ");
+  // could need to set the  alpha value back or it will never decay to
+  simulation.alphaTarget(0);
+  // now at end set the properties back to null
+  d.fx = null;
+  d.fy = null;
 }
