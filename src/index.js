a4aabf40e6a038ddb38a593242c826d8a0e157bd
AR123456 <coloradoz99@gmail.com>
2019-08-18 15:04:52 -0600

    d3 and react scatter plot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import * as d3 from "d3";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+import "./styles.css";
+import Scatterplot from "./Scatterplot";
+import Datapoint from "./Datapoint";
+
+class App extends React.Component {
+  state = {
+    width: 300,
+    height: 300,
+    data: d3.range(100).map(_ => [Math.random(), Math.random()])
+  };
+
+  onClick = () => {
+    const { width, height } = this.state;
+    this.setState({
+      width: width * 0.7,
+      height: height * 0.7
+    });
+  };
+
+  render() {
+    const { width, height, data } = this.state;
+
+    return (
+      <div className="App">
+        <svg width="800" height="800" onClick={this.onClick}>
+          <Scatterplot
+            x={50}
+            y={50}
+            width={width}
+            height={height}
+            data={data}
+            datapoint={({ x, y }) => <Datapoint x={x} y={y} />}
+          />
+        </svg>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
