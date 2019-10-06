e19d31a31d30bc98d32c45bf5b5bef2045e10b34
AR123456 <coloradoz99@gmail.com>
2019-10-06 06:28:01 -0600

    de react 7th grade graphs

--- src/ChartWrapper.js
+++ src/ChartWrapper.js
@@ -1,21 +1,16 @@
+// class based to use lifeccyle methods
+
 import React, { Component } from "react";
+// import the chart from local directory
 import D3Chart from "./D3Chart";
 
 class ChartWrapper extends Component {
+  //passing in referace to where to target on page will be appending visualization to this
+
   componentDidMount() {
-    this.setState({
-      chart: new D3Chart(this.refs.chart)
-    });
+    new D3Chart(this.refs.chart);
   }
-  // this forces react to let D3 handle ths update
-  shouldComponentUpdate() {
-    return false;
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.state.chart.update(nextProps.gender);
-  }
-
+  // using ref system to keep track of the div
   render() {
     return <div ref="chart"></div>;
   }
