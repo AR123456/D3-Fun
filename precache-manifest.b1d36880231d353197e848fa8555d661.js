a6e06e8e47741b480b68d375e68ecfe597abb34e
AR123456 <coloradoz99@gmail.com>
2020-01-02 07:59:50 -0700

    Updates

---
+++ precache-manifest.b1d36880231d353197e848fa8555d661.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd6f3127301f466badb2a262b03ef9b1",
+    "url": "/D3-Fun/index.html"
+  },
+  {
+    "revision": "233276fc5aa8f0c6416f",
+    "url": "/D3-Fun/static/css/main.791b2d09.chunk.css"
+  },
+  {
+    "revision": "26dcabfeb83ce87fc731",
+    "url": "/D3-Fun/static/js/2.c1076594.chunk.js"
+  },
+  {
+    "revision": "233276fc5aa8f0c6416f",
+    "url": "/D3-Fun/static/js/main.6e94e6f4.chunk.js"
+  },
+  {
+    "revision": "2ae0b5b73211ac9c07f7",
+    "url": "/D3-Fun/static/js/runtime-main.b680bd27.js"
+  }
+]);
