d1afc8f2f7d7799e7aee9e43e19f394ad341bbb7
AR123456 <coloradoz99@gmail.com>
2020-01-03 06:25:53 -0700

    curved lin chart with hupdate on click

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-// https://www.youtube.com/watch?v=9uEmNgHzPhQ&t=257s
+// https://www.youtube.com/watch?v=hR8xtl_IbCw
 import React, { useRef, useEffect, useState } from "react";
 import "./App.css";
-import { select } from "d3";
+// adding line and curvedCardian for the line
+import { select, line, curveCardinal } from "d3";
 
 function App() {
-  // define the data array
-  const [data, setData] = useState([25, 30, 45, 60, 20]);
+  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
   const svgRef = useRef();
   useEffect(() => {
-    // select makes the SVG avalible to d3
     const svg = select(svgRef.current);
-    // render circles based on data in array
+    // using line from D3
+    const myLine = line()
+      // tell line based on data where to render each dot on line, based on value index in data array use x to be the index of the value
+      .x((value, index) => index * 50)
+      // y will be the value of the element in the array
+      .y(value => 150 - value)
+      // thi makes a nice curved line
+      .curve(curveCardinal);
+    // select the path elements in svg and sync with data
     svg
-      // select all circles found in svg
-      .selectAll("circle")
-      // sync data with circle
-      .data(data)
-      //define the enter callback , append the circle and define with update what to do
-      .join("circle")
-      // join is handeling entering and updating elements so we do not have to repeat
-      .attr("r", value => value)
-      .attr("cx", value => value * 2)
-      .attr("cy", value => value * 2)
-      .attr("stroke", "red");
+      .selectAll("path")
+      // need to wrap array in another array so d3 doesnt generate a new path element for every element in the array.  We just need one path of all the elements
+      .data([data])
+      // create update remove with new join api
+      .join("path")
+      // attahced the attribute for every entering and updating element
+      // value is a callback in this case- gets the entier data array as an argument foward to myLine
+      // returns the d attribute and attahces it
+      .attr("d", value => myLine(value))
+      // clean up with no fill  and stoke bule
+      .attr("fill", "none")
+      .attr("stroke", "blue");
   }, [data]);
   return (
     <React.Fragment>
-      <svg ref={svgRef}></svg>
+      <svg ref={svgRef}>
+        {/* <path d="M0,150,100,100,150,120" stroke="blue" fill="none" /> */}
+      </svg>
       <br />
       <button onClick={() => setData(data.map(value => value + 5))}>
         Update data
