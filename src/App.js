f04744fa9626c570ac82c89c501b191ea93a8c3b
AR123456 <coloradoz99@gmail.com>
2020-01-08 05:42:59 -0700

    Movie timeline

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-//https://www.youtube.com/watch?v=a4rstx9Pz2o&t=3s
-import React, { useState } from "react";
-import BarChart from "./BarChart";
+import React, { useEffect, useState } from "react";
+import BBTimeline from "./BBTimeline";
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState([25, 30, 45, 60, 10, 65, 75]);
+  const [bbEpisodes, setBbEpisodes] = useState([]);
+  const [bbCharacters, setBbCharacters] = useState([]);
+  const [highlight, setHighlight] = useState();
+
+  useEffect(() => {
+    fetch("https://www.breakingbadapi.com/api/characters?category=Breaking+Bad")
+      .then(response => response.ok && response.json())
+      .then(characters => {
+        setBbCharacters(
+          characters.sort((a, b) => a.name.localeCompare(b.name))
+        );
+      })
+      .catch(console.error);
+  }, []);
+
+  useEffect(() => {
+    fetch("https://www.breakingbadapi.com/api/episodes?series=Breaking+Bad")
+      .then(response => response.ok && response.json())
+      .then(episodes => {
+        console.warn(episodes);
+        setBbEpisodes(episodes);
+      })
+      .catch(console.error);
+  }, []);
 
   return (
     <React.Fragment>
-      {/* passing data above into the BarChart component  */}
-      <BarChart data={data} />
-      <button onClick={() => setData(data.map(value => value + 5))}>
-        Update data
-      </button>
-      <button onClick={() => setData(data.filter(value => value < 35))}>
-        Filter data
-      </button>
-      <button
-        onClick={() => setData([...data, Math.round(Math.random() * 100)])}
-      >
-        Add data
-      </button>
+      <h1>Breaking Bad Timeline</h1>
+      <BBTimeline highlight={highlight} data={bbEpisodes} />
+
+      <h2>Select your character</h2>
+      <select value={highlight} onChange={e => setHighlight(e.target.value)}>
+        <option>Select character</option>
+        {bbCharacters.map(character => (
+          <option key={character.name}>{character.name}</option>
+        ))}
+      </select>
     </React.Fragment>
   );
 }
