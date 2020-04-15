15751136dd670f1d80d8f688832914ac12f57858
AR123456 <coloradoz99@gmail.com>
2020-04-15 05:56:00 -0600

    react line elipse

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Some D3 Data Vis work with and without react
-
-# see it at https://powerful-bastion-30341.herokuapp.com/
+# Using D3 with react hooks to make data vis
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RecLineElipse from "./RecLineElipse";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <RecLineElipse />
+    </React.Fragment>
+  );
+}
+
+export default App;
