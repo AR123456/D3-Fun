d2e66a0b9904cb78d62a2e56727a5ca84eedd880
AR123456 <coloradoz99@gmail.com>
2020-04-16 05:45:57 -0600

    data from file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from "react";
-import RecLineElipse from "./RecLineElipse";
+import React, { useState } from "react";
+import Circles from "./Circles";
 import "./App.css";
 
 function App() {
+  // https://swizec.com/blog/building-a-react-dataviz-with-react-hooks/swizec/8801
+  //  https://swizec1.teachable.com/courses/447741/lectures/9911350
+  const [data] = useState([
+    {
+      name: "Tony",
+      age: "10",
+    },
+    {
+      name: "Jessica",
+      age: "12",
+    },
+    {
+      name: "Andrew",
+      age: "9",
+    },
+    {
+      name: "Emily",
+      age: "10",
+    },
+    {
+      name: "Richard",
+      age: "11",
+    },
+  ]);
+
+  console.log(data);
+  console.log(data[0].name);
   return (
     <React.Fragment>
-      <RecLineElipse />
+      <Circles data={data} />
     </React.Fragment>
   );
 }
