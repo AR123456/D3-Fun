2b26f09df5eb2480950c3e522d8559ab6fa32a53
AR123456 <coloradoz99@gmail.com>
2020-05-01 05:14:00 -0600

    D3 React Margin Groups

--- src/Buildings.js
+++ src/Buildings.js
@@ -6,77 +6,58 @@ function Buildings({ data }) {
   const svgRef = useRef();
   const wrapperRef = useRef();
   const dimensions = useResizeObserver(wrapperRef);
+  const margin = { left: 100, right: 10, top: 10, bottom: 100 };
+  const width = 600 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
   useEffect(() => {
-    const svg = select(svgRef.current);
+    const svg = select(svgRef.current)
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
     if (!dimensions) return;
     data.forEach((d) => {
       d.height = +d.height;
     });
-    // changing x scale width - need to make the bands more narrow or the SVG bigger for all to fit
-    // descrete domain, continuous range
     const x = scaleBand()
-      // space out bands of equal length based on how many elements it has in its domain
-      // cannot add new values in the same way as ordinal scale
-      // categories to display for domain - needed to add to building.json and here in the domain
       .domain(
-        // map function to generate the array of building names
-        // to add to the domain
-        // useful for band scales and ordinal scales category values
         data.map((d) => {
           return d.name;
         })
       )
-      // the pixle postions that we want chart spaced over
-      // span of SVG
-      //range method takes in the min and max of the range
-      .range([0, 400])
-      // padding ratio inbetween the bars in the domain -
+      .range([0, width])
       .paddingInner(0.3)
-      // padding ratio the outside edge of the whole domain
       .paddingOuter(0.3);
-    console.log(x("Burj Khalifa"));
 
     const y = scaleLinear()
-      //domain method, takes in the vaule of the min and max of the domain
-      // 0 to the height of the tallest building
       .domain([
         0,
-        // d3 max takes in an array of data as its first argument
-        // the second argument is pointing to the buildings height value
-        // finds the max value in the array to use in the domain function
         max(data, (d) => {
           return d.height;
         }),
       ])
-      //range method takes in the min and max of the range
-      .range([0, 400]);
+      .range([0, height]);
 
     svg
       .selectAll("rectangle")
       .data(data)
       .join("rect")
       .attr("y", 0)
-      // return the value from passing the building name into the x scale
       .attr("x", (d, i) => {
         return x(d.name);
       })
-
       .attr("width", x.bandwidth)
       .attr("height", (d) => {
-        // pass raw hight values into the scales function before returning
-        // as value of the height attribute
         return y(d.height);
       })
-
-      // .style("fill", "black");
       .attr("fill", (d) => {
         return "grey";
       });
-  }, [data, dimensions]);
+  }, [data, dimensions, height, width, margin]);
   return (
     <React.Fragment>
-      <div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
+      <div ref={wrapperRef}>
         <svg ref={svgRef}></svg>
       </div>
     </React.Fragment>
