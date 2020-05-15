c9148bb4a1c5582ac0a23c60756bd5a9bbe0e8ef
AR123456 <coloradoz99@gmail.com>
2020-05-15 06:11:51 -0600

    ran build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import Video from "./Video";
+
 import "./App.css";
 import BrushChart from "./BrushChart";
 import BrushChartChild from "./BrushChartChild";
